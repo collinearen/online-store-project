be8db5756eb036817739b4b8c1b22cf2d360d8d8
collinear <118291046+collinearen@users.noreply.github.com>
2023-09-12 17:45:46 +0300

    6th commit, last for today I picked up the models, registered them in the admin panel, looped through the objects in the tables, revived the html pages, worked with media files, created database fixtures

--- store/products/views.py
+++ store/products/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
 
 def index(request):
-    context = {
-        'name_store': "Coffee Lake"}
-    return render(request=request, template_name="products/index.html", context=context)
+    return render(request=request, template_name="products/index.html")
 
 
 def products(request):
-    return render(request=request, template_name='products/products.html')
+    context = {
+        'name_store': "Coffee Lake",
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request=request, template_name='products/products.html', context=context)
--- store/store/settings.py
+++ store/store/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -114,6 +114,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
