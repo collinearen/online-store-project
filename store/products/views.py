2f1c397ffc856fc4b3bfbfa7b091ca0e5a520524
collinear <118291046+collinearen@users.noreply.github.com>
2023-09-12 14:40:53 +0300

    FOUR COMMITS. in this commit we added two html pages, connected statics, added staticfiles_dirs, added two controllers that directly display our two html pages

--- store/products/views.py
+++ store/products/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request=request, template_name="products/index.html")
+
+
+def products(request):
+    return render(request=request, template_name='products/products.html')
