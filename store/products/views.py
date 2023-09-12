1cc683352c1bb98c41ad96ee179634dbf2643d79
collinear <118291046+collinearen@users.noreply.github.com>
2023-09-12 15:36:47 +0300

    five commit // here we have already implemented the dry principle in the templates, expanded them with base.html, and worked a little with controllers

--- store/products/views.py
+++ store/products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request=request, template_name="products/index.html")
+    context = {
+        'name_store': "Coffee Lake"}
+    return render(request=request, template_name="products/index.html", context=context)
 
 
 def products(request):
