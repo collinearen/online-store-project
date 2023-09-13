4c63748b60544f5f17814e44b42693748767176d
collinear <118291046+collinearen@users.noreply.github.com>
2023-09-13 22:32:08 +0300

    9th commit

--- store/store/urls.py
+++ store/store/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from products.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
     path('', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users'))
 ]
