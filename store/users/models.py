77b178113bcf5bfb00ad64ce3b80a77c363a47fc
collinear <118291046+collinearen@users.noreply.github.com>
2023-09-13 21:35:04 +0300

    8th commit Here we have already expanded the user model, added two html pages, added controllers to them, connected some buttons

--- store/users/models.py
+++ store/users/models.py
@@ -1,1 +1,6 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', null=True, blank=True)
--- store/users/views.py
+++ store/users/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def login(request):
+    return render(request=request, template_name="login.html")
+
+
+def register(request):
+    return render(request=request, template_name="register.html")
